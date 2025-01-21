a75ee52cfad4aa59bf59347de2b971438d1e45a5
Shane Wade <shanebramble96@hotmail.com>
2025-01-20 19:50:04 -0600

    serving a static HTML file in an Express app

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Converting the __dirname equivalent to ESM.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
