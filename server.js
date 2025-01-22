e70f88e1aad454871425543a813025c522fb0ca2
Shane Wade <shanebramble96@hotmail.com>
2025-01-21 19:42:29 -0600

    create a get request to load all saved notes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+// import db from "./db/db.json";
+// import db from "./db/db.json" assert { type: "json" };
 
 // Converting the __dirname CommonJs equivalent to ESM.
 const __filename = fileURLToPath(import.meta.url);
@@ -10,12 +12,25 @@ const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static("public"));
 
-// Get current saved notes. 
-app.get("/api/notes", (req,res)=> {
-  res.json(); 
+// Get saved notes.
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading JSON file");
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.setHeader("Content-Type", "application/json");
+      res.send(jsonData);
+    } catch (parseErr) {
+      res.status(500).send("Error parsing JSON data");
+    }
+  });
 });
-
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
