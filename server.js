af507e784f3f691f1df53de3b6f05c84c64eb04a
Shane Wade <shanebramble96@hotmail.com>
2025-01-21 18:12:24 -0600

    started to create GET api routes

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-// Converting the __dirname equivalent to ESM.
+// Converting the __dirname CommonJs equivalent to ESM.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Get current saved notes. 
+app.get("/api/notes", (req,res)=> {
+  res.json(); 
+});
+
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
