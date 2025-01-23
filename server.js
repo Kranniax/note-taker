8dfe0a009383d55ab874a3bc1729e456664d3b59
Shane Wade <shanebramble96@hotmail.com>
2025-01-22 19:27:49 -0600

    save notes post requests are now being saved to the db.json file

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-// import db from "./db/db.json";
-// import db from "./db/db.json" assert { type: "json" };
+import { v4 as uuidv4 } from "uuid";
 
 // Converting the __dirname CommonJs equivalent to ESM.
 const __filename = fileURLToPath(import.meta.url);
@@ -12,7 +11,12 @@ const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.static("public"));
+// parse incomming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incomming JSON data
+app.use(express.json());
 
 // Get saved notes.
 app.get("/api/notes", (req, res) => {
@@ -23,6 +27,7 @@ app.get("/api/notes", (req, res) => {
     }
 
     try {
+      // converts data file to JavaScript object.
       const jsonData = JSON.parse(data);
       res.setHeader("Content-Type", "application/json");
       res.send(jsonData);
@@ -31,11 +36,42 @@ app.get("/api/notes", (req, res) => {
     }
   });
 });
+// Post a note
+app.post("/api/notes", (req, res) => {
+  // Validate request body
+  if (!req.body.title || !req.body.text) {
+    return res.status(400).json({ error: "Title and text are required" });
+  }
+  // Add a unique id to notes.
+  req.body.id = uuidv4();
+  // Add timestamps to notes. (Optional)
+  req.body.createdAt = new Date().toISOString();
 
+  // Read json file and then convert to Javascript Object notation.
+  fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+    if (err) throw err;
+    const jsonData = JSON.parse(data);
+    // Add req.body to json file.
+    jsonData.push(req.body);
+
+    // Update and write json file.
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(jsonData, null, 2),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    // Send success response
+    res.status(201).json(req.body);
+  });
+});
+
+// Serve notes page to client side.
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-
+// Serve main index page to client side/ any other route is requested.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
